629d3647200f0d2d5b1a5e358d224b244bf10463
baimao0331 <92855082+baimao0331@users.noreply.github.com>
2024-11-01 01:52:00 +0800

    Delete mid directory

--- mid/js/jugement.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('vergil').addEventListener('ended',jumper,false);
-function jumper(e) {
-    if (window.history.length==1){
-        window.close();
-    }else{
-        window.history.back();
-    }
-}
